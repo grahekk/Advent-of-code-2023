1f104b9e5012d0527784a819e67f318cb9e2446f
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-12-01 10:34:29 +0100

    Update calibration_values_sum.py
    Scratch idea added

--- scripts/day_01/calibration_values_sum.py
+++ scripts/day_01/calibration_values_sum.py
@@ -1,3 +1,12 @@
 import requests
+import re
 
-data = "https://adventofcode.com/2023/day/1/input"
+puzzle_input = "https://adventofcode.com/2023/day/1/input"
+
+test_input = ["1abc2","pqr3stu8vwx","a1b2c3d4e5f","treb7uchet"]
+
+
+for i in test_input:
+    first_digit = re.search(r'\d+', i).group()
+    print(i)
+    print(first_digit)
