7ed0b6582ca9d27a2739f05b348eb43afc179da1
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-12-01 14:49:03 +0100

    Update second_part.py

--- scripts/day_01/second_part.py
+++ scripts/day_01/second_part.py
@@ -1,2 +1,16 @@
 
+test_input = ["two1nine",
+"eightwothree",
+"abcone2threexyz",
+"xtwone3four",
+"4nineeightseven2",
+"zoneight234",
+"7pqrstsixteen"]
+
 num_dict = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
+
+for i in test_input:
+    member_check = [j in i for j in num_dict]
+    print(i, member_check)
+
+print(member_check)
