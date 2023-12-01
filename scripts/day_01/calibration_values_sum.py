8389542102b83cb982ce3e0f9347525d77b5a900
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-12-01 14:40:57 +0100

    First part done

--- scripts/day_01/calibration_values_sum.py
+++ scripts/day_01/calibration_values_sum.py
@@ -5,13 +5,10 @@ puzzle_input_url = "https://adventofcode.com/2023/day/1/input"
 test_input = ["1abc2","pqr3stu8vwx","a1b2c3d4e5f","treb7uchet"]
 input_file = "C:/Users/nikol/Documents/GitHub/Advent-of-code-2023/scripts/day_01/input.txt"
 
-def get_calibration_values(puzzle_input):
+def get_calibration_values_1(puzzle_input):
     for i in puzzle_input:
-        i = i.strip()
-        i = "".join(i.split())
-        print(i)
-        first_digit = re.search(r'\d+', i).group()
-        last_digit = re.search(r'\d+', i[::-1]).group()[::-1]
+        first_digit = re.search(r'\d', i).group()
+        last_digit = re.search(r'\d', i[::-1]).group()[::-1]
         calibration_value = int(f"{first_digit}{last_digit}")
         yield calibration_value
 
@@ -21,14 +18,13 @@ def get_puzzle_input_request(url):
 
 def get_puzzle_input_file(file):
     with open(file) as input_file:
-        data = input_file.readlines()
+        data = input_file.read().splitlines()
         return data
 
 
 if __name__ == "__main__":
+    # first part
     puzzle_input = get_puzzle_input_file(input_file)
     cv = get_calibration_values(puzzle_input)
-    # print(puzzle_input)
-    for i in cv:
-        print(i)
     print(sum(cv))
+    # second part
