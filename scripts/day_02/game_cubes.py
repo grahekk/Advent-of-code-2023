a3c90b92fd7f99025d6a83f5179736b59d61546d
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-12-02 22:27:14 +0100

    Update game_cubes.py

--- scripts/day_02/game_cubes.py
+++ scripts/day_02/game_cubes.py
@@ -1,10 +1,50 @@
+import re
 
-test_games = """
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+test_games = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """
 
-game_id = test_games.find(r"Game\s+")
+possible_ids = []
+games = test_games.strip().split(f"Game ")
+games = games[1:]
+
+for i in games:
+    game = i.replace(':',';')
+    game = game.split(';')
+    print(game)
+    for j in game:
+        pairs = j.split(',')
+        print(pairs)
+        red_cubes = 0
+        green_cubes = 0
+        blue_cubes = 0
+        for p in pairs:
+            # print(p)
+            if "blue" in p:
+                blue_cubes = int(re.search(r'\d+', p).group())
+                print(blue_cubes)
+            elif "red" in p:
+                red_cubes = int(re.search(r'\d+', p).group())
+                print(red_cubes)
+            elif "green" in p:
+                green_cubes = int(re.search(r'\d+', p).group())
+                print(green_cubes)
+            else:
+                game_id = int(p)
+
+        if red_cubes > 12 or green_cubes > 13 or blue_cubes > 15:
+            possibility = False
+            print(f"skipping game id {game_id}")
+            break
+        else:
+            possibility = True
+    if possibility == True:
+        possible_ids.append(game_id)
+        # print(possible_ids)
+
+print(possible_ids)
+print(sum(set(possible_ids)))
+            
