d4c1c71258a1486195a3c81484c250529176f2b4
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-12-03 11:52:51 +0100

    second part added for day2

--- scripts/day_02/game_cubes.py
+++ scripts/day_02/game_cubes.py
@@ -7,6 +7,7 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """
 input_file = "input.txt"
+input_file = "scripts\day_02\input.txt"
 with open(input_file) as file:
     data = file.read().splitlines()
 
@@ -17,42 +18,83 @@ possible_ids = []
 
 games = data
 
-for i in games:
-    game = i.replace(':',';')
-    game = game.split(';')
-    print(game)
-    for j in game:
-        pairs = j.split(',')
-        print(pairs)
-        red_cubes = 0
-        green_cubes = 0
-        blue_cubes = 0
-        for p in pairs:
-            # print(p)
-            if "blue" in p:
-                blue_cubes = int(re.search(r'\d+', p).group())
-                print(blue_cubes)
-            elif "red" in p:
-                red_cubes = int(re.search(r'\d+', p).group())
-                print(red_cubes)
-            elif "green" in p:
-                green_cubes = int(re.search(r'\d+', p).group())
-                print(green_cubes)
-            elif "Game":
-                # game_id = int(p)
-                game_id = int(re.search(r'\d+', p).group())
+def part_1():
+    for i in games:
+        game = i.replace(':',';')
+        game = game.split(';')
+        print(game)
+        for j in game:
+            pairs = j.split(',')
+            print(pairs)
+            red_cubes = 0
+            green_cubes = 0
+            blue_cubes = 0
+            for p in pairs:
+                # print(p)
+                if "blue" in p:
+                    blue_cubes = int(re.search(r'\d+', p).group())
+                    print(blue_cubes)
+                elif "red" in p:
+                    red_cubes = int(re.search(r'\d+', p).group())
+                    print(red_cubes)
+                elif "green" in p:
+                    green_cubes = int(re.search(r'\d+', p).group())
+                    print(green_cubes)
+                elif "Game":
+                    # game_id = int(p)
+                    game_id = int(re.search(r'\d+', p).group())
 
+            if red_cubes > 12 or green_cubes > 13 or blue_cubes > 14:
+                possibility = False
+                print(f"skipping game id {game_id}")
+                break
+            else:
+                possibility = True
+        if possibility == True:
+            possible_ids.append(game_id)
+            # print(possible_ids)
 
-        if red_cubes > 12 or green_cubes > 13 or blue_cubes > 15:
-            possibility = False
-            print(f"skipping game id {game_id}")
-            break
-        else:
-            possibility = True
-    if possibility == True:
-        possible_ids.append(game_id)
-        # print(possible_ids)
-
-print(possible_ids)
-print(sum(set(possible_ids)))
+    print(possible_ids)
+    print(sum(possible_ids))
             
+def part_2():
+    for i in games:
+        game = i.replace(':',';')
+        game = game.split(';')
+        print(game)
+        for j in game:
+            pairs = j.split(',')
+            print(pairs)
+            red_cubes = 0
+            green_cubes = 0
+            blue_cubes = 0
+            for p in pairs:
+                # print(p)
+                if "blue" in p:
+                    blue_cubes = int(re.search(r'\d+', p).group())
+                    
+                elif "red" in p:
+                    red_cubes = int(re.search(r'\d+', p).group())
+                    print(red_cubes)
+                elif "green" in p:
+                    green_cubes = int(re.search(r'\d+', p).group())
+                    print(green_cubes)
+                elif "Game":
+                    # game_id = int(p)
+                    game_id = int(re.search(r'\d+', p).group())
+
+            if red_cubes > 12 or green_cubes > 13 or blue_cubes > 14:
+                possibility = False
+                print(f"skipping game id {game_id}")
+                break
+            else:
+                possibility = True
+        if possibility == True:
+            possible_ids.append(game_id)
+            # print(possible_ids)
+
+    print(possible_ids)
+    print(sum(possible_ids))
+
+if __name__ == "__main__":
+    part_2()
